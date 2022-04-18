1e02098c668841f3ab6bce99744f6c3f3b382b56
Patrick DeVivo <patrick.devivo@gmail.com>
2022-04-18 09:58:00 -0400

    doc: tweak README, add some examples

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 # fluentgraphql
 
 This package wraps the [graphql-go/graphql](https://github.com/graphql-go/graphql) implementation to provide a "fluent" pattern for constructing GraphQL queries in Go.
-This can be valuable in situations where *dynamic* queries are desired: when the fields of a query are not known until runtime.
+This can be valuable in situations where *dynamic* queries are desired: when the fields of a GraphQL query (or mutation) are not known until runtime.
 For most other use cases, plain query strings or a helper library such as [this](https://github.com/shurcooL/graphql) should be sufficient.
 
 ```golang
@@ -16,6 +16,11 @@ import (
 )
 
 func main() {
-    fgql.NewQuery().Scalar("hello").Root().String() // { hello }
+    fgql.NewQuery().Scalar("hello").Root().String()
+    /*
+		{
+			hello
+		}
+	*/
 }
 ```
--- examples/simple/main.go
+++ examples/simple/main.go
@@ -3,82 +3,42 @@ package main
 import (
 	"fmt"
 
-	"github.com/graphql-go/graphql/language/ast"
-	"github.com/graphql-go/graphql/language/printer"
-	fgl "github.com/mergestat/fluentgraphql"
+	fgql "github.com/mergestat/fluentgraphql"
 )
 
 func main() {
-	s := fgl.NewQuery(fgl.WithName("MyQuery"), fgl.WithVariableDefinitions(
-		fgl.NewVariableDefinition("var1", "string", false, fgl.NewBooleanValue(true)),
-	)).
-		Scalar("hello", fgl.WithAlias("alias")).
-		Scalar("world").
-		Selection("object", fgl.WithArguments(
-			fgl.NewArgument("int", fgl.NewIntValue(123)),
-			fgl.NewArgument("float", fgl.NewFloatValue(123.102434)),
-			fgl.NewArgument("string", fgl.NewStringValue("string-arg")),
-			fgl.NewArgument("boolean", fgl.NewBooleanValue(false)),
-			fgl.NewArgument("enum", fgl.NewEnumValue("SOME_ENUM")),
-			fgl.NewArgument("list", fgl.NewListValue(
-				fgl.NewStringValue("str1"),
-				fgl.NewStringValue("str2"),
-				fgl.NewStringValue("str3"),
-			)),
-			fgl.NewArgument("object", fgl.NewObjectValue(map[string]*fgl.Value{
-				"field1": fgl.NewStringValue("str1"),
-				"field2": fgl.NewIntValue(123),
-			})),
-			fgl.NewArgument("var", fgl.NewVariableValue("someVar")),
+	a := fgql.NewQuery().Scalar("hello").Root().String()
+
+	/*
+		{
+			hello
+		}
+	*/
+	fmt.Println(a)
+
+	b := fgql.NewQuery().Scalar("hello").Scalar("world").Root().String()
+
+	/*
+		{
+			hello
+			world
+		}
+	*/
+	fmt.Println(b)
+
+	c := fgql.NewQuery(
+		fgql.WithName("MyQuery"), fgql.WithVariableDefinitions(fgql.NewVariableDefinition("myVar", "string", true, nil))).
+		Scalar("hello", fgql.WithAlias("myAlias"), fgql.WithArguments(
+			fgql.NewArgument("anArg", fgql.NewVariableValue("myVar")),
 		)).
-		Scalar("patrick").Parent().
-		Selection("world").Scalar("hello").InlineFragment("User").Scalar("fragmentField").Selection("patrick").Scalar("devivo").
-		Root().Fragment("comparisonFields", "Character").Scalar("name").Selection("friendsConnection", fgl.WithArguments(
-		fgl.NewArgument("first", fgl.NewVariableValue("first")),
-	)).Scalar("totalCount").Selection("edges").Selection("node").Scalar("name").
+		Scalar("world").
 		Root().String()
 
-	fmt.Println(s)
-
-	a := ast.NewOperationDefinition(&ast.OperationDefinition{
-		Operation: ast.OperationTypeMutation,
-		Name:      ast.NewName(&ast.Name{Value: ""}),
-		SelectionSet: ast.NewSelectionSet(&ast.SelectionSet{
-			Selections: []ast.Selection{
-				ast.NewField(&ast.Field{
-					Name: ast.NewName(&ast.Name{Value: "hello"}),
-				}),
-				ast.NewField(&ast.Field{
-					Name: ast.NewName(&ast.Name{Value: "world"}),
-					Arguments: []*ast.Argument{
-						ast.NewArgument(&ast.Argument{
-							Name:  ast.NewName(&ast.Name{Value: "arg1"}),
-							Value: ast.NewIntValue(&ast.IntValue{Value: "123"}),
-						}),
-					},
-				}),
-				ast.NewField(&ast.Field{
-					Name: ast.NewName(&ast.Name{Value: "author"}),
-					SelectionSet: ast.NewSelectionSet(&ast.SelectionSet{
-						Selections: []ast.Selection{
-							ast.NewInlineFragment((&ast.InlineFragment{
-								TypeCondition: ast.NewNamed(&ast.Named{Name: ast.NewName(&ast.Name{Value: "User"})}),
-								SelectionSet: ast.NewSelectionSet(&ast.SelectionSet{
-									Selections: []ast.Selection{
-										ast.NewField(&ast.Field{
-											Name: ast.NewName(&ast.Name{Value: "userField"}),
-										}),
-									},
-								}),
-							})),
-						},
-					}),
-				}),
-			},
-		}),
-	})
-
-	printer.Print(a)
-	// fmt.Println(printer.Print(a))
-
+	/*
+		query MyQuery($myVar: string!) {
+			myAlias: hello(anArg: $myVar)
+			world
+		}
+	*/
+	fmt.Println(c)
 }
