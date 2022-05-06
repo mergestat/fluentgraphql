96854db1a70a91f6169025cbd5503bffcece0e07
Patrick DeVivo <patrick.devivo@gmail.com>
2022-05-06 16:54:13 -0400

    chore: implement additional test cases

--- fluentgraphql_test.go
+++ fluentgraphql_test.go
@@ -32,72 +32,76 @@ func queryMatchesTree(t *testing.T, query string, root ast.Node) string {
 
 func TestSelections(t *testing.T) {
 	for name, testCase := range map[string]struct {
-		wantedQuery string
-		selection   *Selection
+		wanted    string
+		selection *Selection
 	}{
 		"SingleScalar": {
-			wantedQuery: `{ hello }`,
-			selection:   NewQuery().Scalar("hello"),
+			wanted:    `{ hello }`,
+			selection: NewQuery().Scalar("hello"),
 		},
 		"SingleScalarWithIntArgument": {
-			wantedQuery: `{ hello(arg1: 123) }`,
-			selection:   NewQuery().Scalar("hello", WithArguments(NewArgument("arg1", NewIntValue(123)))),
+			wanted:    `{ hello(arg1: 123) }`,
+			selection: NewQuery().Scalar("hello", WithArguments(NewArgument("arg1", NewIntValue(123)))),
 		},
 		"SingleScalarWithFloatArgument": {
-			wantedQuery: `{ hello(arg1: 123.12) }`,
-			selection:   NewQuery().Scalar("hello", WithArguments(NewArgument("arg1", NewFloatValue(123.12)))),
+			wanted:    `{ hello(arg1: 123.12) }`,
+			selection: NewQuery().Scalar("hello", WithArguments(NewArgument("arg1", NewFloatValue(123.12)))),
 		},
 		"SingleScalarWithStringArgument": {
-			wantedQuery: `{ hello(arg1: "string") }`,
-			selection:   NewQuery().Scalar("hello", WithArguments(NewArgument("arg1", NewStringValue("string")))),
+			wanted:    `{ hello(arg1: "string") }`,
+			selection: NewQuery().Scalar("hello", WithArguments(NewArgument("arg1", NewStringValue("string")))),
 		},
 		"SingleScalarWithBooleanArgument": {
-			wantedQuery: `{ hello(arg1: true) }`,
-			selection:   NewQuery().Scalar("hello", WithArguments(NewArgument("arg1", NewBooleanValue(true)))),
+			wanted:    `{ hello(arg1: true) }`,
+			selection: NewQuery().Scalar("hello", WithArguments(NewArgument("arg1", NewBooleanValue(true)))),
 		},
 		"SingleScalarWithEnumArgument": {
-			wantedQuery: `{ hello(arg1: SOME_ENUM) }`,
-			selection:   NewQuery().Scalar("hello", WithArguments(NewArgument("arg1", NewEnumValue("SOME_ENUM")))),
+			wanted:    `{ hello(arg1: SOME_ENUM) }`,
+			selection: NewQuery().Scalar("hello", WithArguments(NewArgument("arg1", NewEnumValue("SOME_ENUM")))),
 		},
 		"SingleScalarWithListArgument": {
-			wantedQuery: `{ hello(arg1: ["some-string", "some-other-string"]) }`,
-			selection:   NewQuery().Scalar("hello", WithArguments(NewArgument("arg1", NewListValue(NewStringValue("some-string"), NewStringValue("some-other-string"))))),
+			wanted:    `{ hello(arg1: ["some-string", "some-other-string"]) }`,
+			selection: NewQuery().Scalar("hello", WithArguments(NewArgument("arg1", NewListValue(NewStringValue("some-string"), NewStringValue("some-other-string"))))),
 		},
 		"SingleScalarWithObjectArgument": {
-			wantedQuery: `{ hello(arg1: { val1: "a", val2: "b"}) }`,
+			wanted: `{ hello(arg1: { val1: "a", val2: "b"}) }`,
 			selection: NewQuery().Scalar("hello", WithArguments(NewArgument("arg1", NewObjectValue(
 				NewObjectValueField("val1", NewStringValue("a")),
 				NewObjectValueField("val2", NewStringValue("b")),
 			)))),
 		},
 		"SingleScalarWithVariableArgument": {
-			wantedQuery: `{ hello(arg1: $var1) }`,
-			selection:   NewQuery().Scalar("hello", WithArguments(NewArgument("arg1", NewVariableValue("var1")))),
+			wanted:    `{ hello(arg1: $var1) }`,
+			selection: NewQuery().Scalar("hello", WithArguments(NewArgument("arg1", NewVariableValue("var1")))),
 		},
 		"SingleScalarWithQueryName": {
-			wantedQuery: `query SomeName { hello }`,
-			selection:   NewQuery(WithName("SomeName")).Scalar("hello"),
+			wanted:    `query SomeName { hello }`,
+			selection: NewQuery(WithName("SomeName")).Scalar("hello"),
 		},
 		"SingleScalarWithAlias": {
-			wantedQuery: `{ someAlias: hello }`,
-			selection:   NewQuery().Scalar("hello", WithAlias("someAlias")),
+			wanted:    `{ someAlias: hello }`,
+			selection: NewQuery().Scalar("hello", WithAlias("someAlias")),
 		},
 		"SingleScalarWithVariable": {
-			wantedQuery: `query($a: string) { hello }`,
-			selection:   NewQuery(WithVariableDefinitions(NewVariableDefinition("a", "string", false, nil))).Scalar("hello"),
+			wanted:    `query($a: string) { hello }`,
+			selection: NewQuery(WithVariableDefinitions(NewVariableDefinition("a", "string", false, nil))).Scalar("hello"),
+		},
+		"SingleScalarWithRequiredVariable": {
+			wanted:    `query($a: string!) { hello }`,
+			selection: NewQuery(WithVariableDefinitions(NewVariableDefinition("a", "string", true, nil))).Scalar("hello"),
 		},
 		"SingleScalarWithVariableAndName": {
-			wantedQuery: `query NamedQuery($a: string) { hello }`,
-			selection:   NewQuery(WithName("NamedQuery"), WithVariableDefinitions(NewVariableDefinition("a", "string", false, nil))).Scalar("hello"),
+			wanted:    `query NamedQuery($a: string) { hello }`,
+			selection: NewQuery(WithName("NamedQuery"), WithVariableDefinitions(NewVariableDefinition("a", "string", false, nil))).Scalar("hello"),
 		},
 		"SubSelection": {
-			wantedQuery: `{ hello { world } }`,
-			selection:   NewQuery().Selection("hello").Scalar("world"),
+			wanted:    `{ hello { world } }`,
+			selection: NewQuery().Selection("hello").Scalar("world"),
 		},
 	} {
 		t.Run(name, func(t *testing.T) {
 			root := testCase.selection.Root()
-			if diff := queryMatchesTree(t, testCase.wantedQuery, root.node); diff != "" {
+			if diff := queryMatchesTree(t, testCase.wanted, root.node); diff != "" {
 				t.Log("produced GraphQL query does not match what's wanted", diff)
 				t.Fatal()
 			}
@@ -107,17 +111,56 @@ func TestSelections(t *testing.T) {
 
 func TestMutations(t *testing.T) {
 	for name, testCase := range map[string]struct {
-		wantedQuery string
-		selection   *Selection
+		wanted    string
+		selection *Selection
 	}{
 		"SingleScalar": {
-			wantedQuery: `mutation { hello }`,
-			selection:   NewMutation().Scalar("hello"),
+			wanted:    `mutation { hello }`,
+			selection: NewMutation().Scalar("hello"),
+		},
+		"SingleScalarWithIntArgument": {
+			wanted:    `mutation { hello(arg1: 123) }`,
+			selection: NewMutation().Scalar("hello", WithArguments(NewArgument("arg1", NewIntValue(123)))),
+		},
+		"SingleScalarWithFloatArgument": {
+			wanted:    `mutation { hello(arg1: 123.12) }`,
+			selection: NewMutation().Scalar("hello", WithArguments(NewArgument("arg1", NewFloatValue(123.12)))),
+		},
+		"SingleScalarWithStringArgument": {
+			wanted:    `mutation { hello(arg1: "string") }`,
+			selection: NewMutation().Scalar("hello", WithArguments(NewArgument("arg1", NewStringValue("string")))),
+		},
+		"SingleScalarWithBooleanArgument": {
+			wanted:    `mutation { hello(arg1: true) }`,
+			selection: NewMutation().Scalar("hello", WithArguments(NewArgument("arg1", NewBooleanValue(true)))),
+		},
+		"SingleScalarWithEnumArgument": {
+			wanted:    `mutation { hello(arg1: SOME_ENUM) }`,
+			selection: NewMutation().Scalar("hello", WithArguments(NewArgument("arg1", NewEnumValue("SOME_ENUM")))),
+		},
+		"SingleScalarWithListArgument": {
+			wanted:    `mutation { hello(arg1: ["some-string", "some-other-string"]) }`,
+			selection: NewMutation().Scalar("hello", WithArguments(NewArgument("arg1", NewListValue(NewStringValue("some-string"), NewStringValue("some-other-string"))))),
+		},
+		"SingleScalarWithObjectArgument": {
+			wanted: `mutation { hello(arg1: { val1: "a", val2: "b"}) }`,
+			selection: NewMutation().Scalar("hello", WithArguments(NewArgument("arg1", NewObjectValue(
+				NewObjectValueField("val1", NewStringValue("a")),
+				NewObjectValueField("val2", NewStringValue("b")),
+			)))),
+		},
+		"SingleScalarWithVariableArgument": {
+			wanted:    `mutation { hello(arg1: $var1) }`,
+			selection: NewMutation().Scalar("hello", WithArguments(NewArgument("arg1", NewVariableValue("var1")))),
+		},
+		"SingleScalarWithQueryName": {
+			wanted:    `mutation SomeName { hello }`,
+			selection: NewMutation(WithName("SomeName")).Scalar("hello"),
 		},
 	} {
 		t.Run(name, func(t *testing.T) {
 			root := testCase.selection.Root()
-			if diff := queryMatchesTree(t, testCase.wantedQuery, root.node); diff != "" {
+			if diff := queryMatchesTree(t, testCase.wanted, root.node); diff != "" {
 				t.Log("produced GraphQL query does not match what's wanted", diff)
 				t.Fatal()
 			}
